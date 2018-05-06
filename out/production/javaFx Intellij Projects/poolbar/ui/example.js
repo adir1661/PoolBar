807c3850f30a4ff45050a71c657ffe75ac262d1e
adir1661 <33230109+adir1661@users.noreply.github.com>
2018-05-06 21:22:53 +0300

    commit

---
+++ out/production/javaFx Intellij Projects/poolbar/ui/example.js
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?language javascript?>
+    <fx:script>
+        function handleSubmitButtonAction() {
+        actiontarget.setText("Calling the JavaScript");
+        }
+    </fx:script>
